511d4061241c0e382beeaa8f0d53d4f165dbac8b
malekidev <sirmahdimaleki@gmail.com>
2025-01-09 03:21:33 +0330

    dockerize-fixed

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // MySQL Connection
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME ,
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
-
 
 db.connect((err) => {
   if (err) {
